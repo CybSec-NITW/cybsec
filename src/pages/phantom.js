da279a3b64bc6ba3848760e7bd4715798d724d8e
Aakash Kumar <aakashkumaar074@gmail.com>
2020-07-24 00:45:29 +0530

    Update phantom.js

--- src/pages/phantom.js
+++ src/pages/phantom.js
@@ -36,14 +36,14 @@ class phantomCTF extends React.Component {
         
               <p>Phantom 3.0 is coming soon!! Pull up your gloves and get ready for Cybsec's biggest and best yet Capture The Flag.</p>
       
-              <p>The D-Day(s) - 15th Aug 6:30pm to 16th Aug 2:30pm </p>
-              <p>Venue - The comforts of your home </p>
-              <p>Necessities - A brain and a laptop</p>
+              <p>The D-Day(s) - 15th Aug 6:30pm to 16th Aug 2:30pm <br></br>
+              Venue - The comforts of your home <br></br>
+              Necessities - A brain and a laptop</p>
 
               <p>For NITW students registration Link - <Link href='http://registration.cybsec.in/'>registration.cybsec.in</Link></p>
 
               <p>For Non-NITW participants - Link will be provided soon.</p>
-              <br>
+              <br></br><br></br><br></br>
           <header>
               <h1><Text>Sponsors</Text></h1>
             </header>
