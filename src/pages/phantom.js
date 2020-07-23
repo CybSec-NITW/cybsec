eca29db9ac6fad9820cd5ec4f377f32b00e69040
Aakash Kumar <aakashkumaar074@gmail.com>
2020-07-24 00:36:15 +0530

    Update phantom.js

--- src/pages/phantom.js
+++ src/pages/phantom.js
@@ -35,9 +35,10 @@ class phantomCTF extends React.Component {
               <p>It is a Jeopardy Style CTF having various types of challenges like web exploitation, forensics, cryptography, binary exploitation, reverse engineering and some miscellaneous.</p>
         
               <p>Phantom 3.0 is coming soon!! Pull up your gloves and get ready for Cybsec's biggest and best yet Capture The Flag.</p>
-              <p>The D-Day(s) - 15th Aug 6:30pm to 16th Aug 2:30pm <br></br>
-                 Venue - The comforts of your home <br></br>
-                 Necessities - A brain and a laptop</p>
+      
+              <p>The D-Day(s) - 15th Aug 6:30pm to 16th Aug 2:30pm </p>
+              <p>Venue - The comforts of your home </p>
+              <p>Necessities - A brain and a laptop</p>
 
               <p>For NITW students registration Link - <Link href='http://registration.cybsec.in/'>registration.cybsec.in</Link></p>
 
