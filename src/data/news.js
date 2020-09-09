465023d37816068f76a2772e08f056c94a9d95d4
Ruchira-111 <53949768+Ruchira-111@users.noreply.github.com>
2020-09-09 21:46:29 +0530

    Update news.js

--- src/data/news.js
+++ src/data/news.js
@@ -2,93 +2,165 @@
 // First items are the latest.
 
 module.exports = [
-                  {
-                  title: '$groot',
-                  message: `I am Groot!!!🙃🙂
-                  Hacker | Coder | Web Developer | Memer | Foodie | Iron Man Fan | Night-Owl | TIFLS | Jack of All Trades
-                  We break security not hearts`,
-                  image: '/images/groot.jpg',
-                  link: 'https://www.aakash-kumar.me'
-                  },
-                  {
-                  title: 'silverstar',
-                  message: `Hello It's silverstar.
-                  The most interesting thing in hacking is the weakest link 🤔`,
-                  image: '/images/silverstar.jpeg',
-                  link: 'https://www.hackthebox.eu/home/users/profile/172291'
-                  },
-                  {
-                  title: 'Lucy',
-                  message: `Hello This is Lucy.
-                  Rote learning brings chaos, not knowledge.
-                  `,
-                  image: '/images/lucy.jpeg',
-                  link: '#'
-                  },
-                  {
-                  title: '$c@rl3t W1tch',
-                  message: `Avenger in the CybSec
-                  Please stand by...
-                  Or get ready to become victim of social engineering
-                  `,
-                  image: '/images/scarlett.jpeg',
-                  link: '#'
-                  },
-                  {
-                  title: 'Splynter',
-                  message: `I ain't what you'd ever anticipate.
-                  Security is MYTH.
-                  `,
-                  image: '/images/splynter.jpeg',
-                  link: '#'
-                  },
-                  {
-                  title: 'achicka',
-                  message: `Hi I'm Achicka.
-                  I don't chicken out.`,
-                  image: '/images/ashiqa.jpeg',
-                  link: '#'
-                  },
-                  {
-                  title: 'BoB',
-                  message: `Bellow... This is BoB.. Buster of Bugs..
-                  My job is thinking out of the bob.`,
-                  image: '/images/bob.jpeg',
-                  link: '#'
-                  },
-                  {
-                  title: 'konichiva',
-                  message: `できないことはできる！バグを食べるのが大好きです。あなたは私が何を計画しているのか分かりません！`,
-                  image: '/images/konichiva.jpeg',
-                  link: '#'
-                  },
-                  {
-                  title: 'M3G4N',
-                  message: `I'm Megan.
-                  Hacking to learn`,
-                  image: '/images/megan.jpeg',
-                  link: '#'
-                  },
-                  {
-                  title: '5and0r D3zt3z',
-                  message: `This is Sandor
-                  I hack for money ...Bug Bounty...`,
-                  image: '/images/sandor.png',
-                  link: '#'
-                  },
-                  {
-                  title: 'tonyq16',
-                  message: `You know who I am.
-                  Currently making scripts to hack you, be prepared`,
-                  image: '/images/tony.jpeg',
-                  link: '#'
-                  },
-                  {
-                  title: 'pica_pica',
-                  message: `Hello this is detective Pikachu
-                  BEWARE! Ur poke balls could be hacked.`,
-                  image: '/images/pica_pica.jpeg',
-                  link: '#'
-                  }
+        {
+            title: ‘$root’,
+            message: ‘I am Groot!!!🙃🙂
+            Hacker | Coder | Web Developer | Memer | Foodie | Iron Man Fan | Night-Owl | TIFLS | Jack of All Trades
+            We break security not hearts’,
+            image: ‘/images/groot.jpg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Sachiko’,
+            message: ‘Hii this is Sachiko
+            Dont be fooled by that face, she will hack you’,
+            image: ‘/images/Sachiko.jpg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Sp1ynt3r’,
+            message: ‘Mind Your own HACKING’,
+            image: ‘/images/Sp1ynt3r.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Phantom King’,
+            message: ‘できないことはできる！バグを食べるのが大好きです。あなたは私が何を計画しているのか分かりません！’,
+            image: ‘/images/Phantom King.jpg’,
+            link: ‘#’
+        },
+        {
+            title: ‘MY$TIQU3’,
+            message: ‘Hey its mystique... 
+            When solving problems, dig at the roots instead of just hacking at the leaves.’,
+            image: ‘/images/MY$TIQU3.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Argenestel’,
+            message: ‘Otaku in hackers realm’,
+            image: ‘/images/Argenestel.jpg’,
+            link: ‘#’
+        },
+        {
+            title: ‘a.chick’,
+            message: ‘I don't chicken out ‘,
+            image: ‘/images/ashiqa.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Dr. Sheldon’,
+            message: ‘Ohh you all must be knowing me. If not, then go away you inferior brains.
+            BTW, I'm batman!!!! Bazinga!!!’,
+            image: ‘/images/Dr. Sheldon.jpg’,
+            link: ‘#’
+        },
+        {
+            title: ‘B.O.B.’,
+            message: ‘ Bellow... This is BoB.. Buster of Bugs..
+            My job is thinking out of the bob.’,
+            image: ‘/images/bob.jpeg’,
+            link: ‘#’
+        },
+        {
+           title: ‘F1R3H3ART’,
+           message: ‘Fae in human disguise.. all cookies accepted😋..               
+           ASSIMILATION:SUCCESS
+           SUBMIT REPORTS:SUBMITTED HUMAN BEHAVIOR 101 TO OVERLORD ZUCC
+           HACK ALL SYSTEMS:ONGOING
+           WORLD DOMINATION:PENDING’,
+           image: ‘/images/F1R3H3ART.jpg’,
+           link: ‘#’
+        },
+        {
+            title: ‘Kakrotgnan’,
+            message: ‘Lemme touch U...
+            Your weakness is my strength 💪 ‘,
+            image: ‘/images/Kakrotgnan.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Pika’,
+            message: ‘Hello this is detective Pikachu
+            BEWARE! Ur poke balls could be hacked.’,
+            image: ‘/images/pica_pica.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Dคяк☬Kห¡gнт’,
+            message: ‘It's Not Who i am underneath ,
+            But What I Hack that defines me’,
+            image: ‘/images/Dคяк☬Kห¡gнт.png’,
+            link: ‘#’
+        }
+        {
+            title: ‘m3g4n’,
+            message: ‘I'm Megan.
+            Hacking to learn’,
+            image: ‘/images/megan.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Tommy shelby’,
+            message: ‘Intelligence is a very valuable thing, innit, my friend? And usually it comes far too   late.’,
+            image: ‘/images/Tommy shelby.jpg’,
+            link: ‘#’
+        },
+        {
+            title: ‘DARK DRAGON’,
+            message: ‘INFOrmation is Wealth’,
+            image: ‘/images/DARK DRAGON.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Purplert’,
+            message: ‘Please stand by...
+            Or get ready to become victim of social engineering’,
+            image: ‘/images/scarlett.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Lucy’,
+            message: ‘Your Ignorance is our power’,
+            image: ‘/images/lucy.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘LakuMigAn0’,
+            message: ‘Hey there! 👋
+            Aspiring Hacker & Coder | Music Love
+            Never tell anyone everything you know... ;) ‘,
+            image: ‘/images/LakuMigAn0.jpeg’,
+            link: ‘#’
+        },
+        {
+            title: ‘TrivalenT’,
+            message: ‘Wubba Lubba Dub Dub 🤤
+            Mixing potions to hack u wid my chemistry 🧙‍♂‍’,
+            image: ‘/images/TrivalenT.jfif’,
+            link: ‘#’
+        },
+        {
+            title: ‘Neo’,
+            message: ‘Hey! I am Neo. Nice to m33t u.’,
+            image: ‘/images/Neo.jpg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Harlequin’,
+            message: ‘Hi, I'm harlequin.
+            Don't hate the hacker, hate the code.’,
+            image: ‘/images/Harlequin.jpg’,
+            link: ‘#’
+        },
+        {
+            title: ‘Entwicklerish’,
+            message: ‘Sugar rush proximity detected, how would you like to proceed?’,
+            image: ‘/images/Entwicklerish.png’,
+            link: ‘#’
+        }
+
+  
+  
 
 ];
