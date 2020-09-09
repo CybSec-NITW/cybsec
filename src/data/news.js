615d59fe5640e03e60cc50fbfcf58faacd59a3ce
Aakash Kumar <aakashkumaar074@gmail.com>
2020-09-10 01:06:52 +0530

    Update news.js

--- src/data/news.js
+++ src/data/news.js
@@ -4,7 +4,7 @@
 module.exports = [
         {
             title: '$root',
-            message:'I am Groot!!!🙃🙂\nHacker | Coder | Web Developer | Programmer | Memer | Foodie | Omniscient HackTheBox | Iron Man Fan | Night-Owl | TIFLS | Interested in Astrophysycs | Jack of All Trades | South Delhi | O -ve\nWe break security not hearts',
+            message:'I am Groot!!!🙃🙂\nHacker | Coder | Web Developer | Programmer | Memer | Foodie | Omniscient HackTheBox | Iron Man Fan | Night-Owl | TIFLS | Interested in Astrophysics | Jack of All Trades | South Delhi | O -ve\nWe break security not hearts',
             image:'/images/groot.jpg',
             link:'#'
         },
