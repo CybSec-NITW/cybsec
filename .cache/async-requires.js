ae9a4a4b01f4fde69065271b49e127907fc9f767
Shubh_ank <62099471+5h0bh4nk@users.noreply.github.com>
2021-02-08 19:28:27 +0530

    phantom4 details update

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,13 +2,13 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---src-pages-404-js": () => import("/Users/aakash/Desktop/cybsec/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-about-js": () => import("/Users/aakash/Desktop/cybsec/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-files-js": () => import("/Users/aakash/Desktop/cybsec/src/pages/files.js" /* webpackChunkName: "component---src-pages-files-js" */),
-  "component---src-pages-index-js": () => import("/Users/aakash/Desktop/cybsec/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-phantom-js": () => import("/Users/aakash/Desktop/cybsec/src/pages/phantom.js" /* webpackChunkName: "component---src-pages-phantom-js" */),
-  "component---src-pages-team-js": () => import("/Users/aakash/Desktop/cybsec/src/pages/team.js" /* webpackChunkName: "component---src-pages-team-js" */)
+  "component---src-pages-404-js": () => import("C:\\Users\\hp\\Desktop\\cybsec\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("C:\\Users\\hp\\Desktop\\cybsec\\src\\pages\\about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-files-js": () => import("C:\\Users\\hp\\Desktop\\cybsec\\src\\pages\\files.js" /* webpackChunkName: "component---src-pages-files-js" */),
+  "component---src-pages-index-js": () => import("C:\\Users\\hp\\Desktop\\cybsec\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-phantom-js": () => import("C:\\Users\\hp\\Desktop\\cybsec\\src\\pages\\phantom.js" /* webpackChunkName: "component---src-pages-phantom-js" */),
+  "component---src-pages-team-js": () => import("C:\\Users\\hp\\Desktop\\cybsec\\src\\pages\\team.js" /* webpackChunkName: "component---src-pages-team-js" */)
 }
 
-exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/aakash/Desktop/cybsec/.cache/data.json")
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "C:\\Users\\hp\\Desktop\\cybsec\\.cache\\data.json")
 
