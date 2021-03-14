3ca84561e8a2c6291288b843e0df5b4c4f69db63
Ruchira-111 <53949768+Ruchira-111@users.noreply.github.com>
2021-03-14 22:53:55 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,43 +2,45 @@ module.exports = [
   
          {
             title:'Cynux Security OS',
-            description:'Cynux is a Linux based distribution curated for pentesters with more than 200 tools and a great interface. We also keep giving regular updates so stay tuned! ',
-            image:
+            message:'Cynux is a Linux based distribution curated for pentesters with more than 200 tools and a great interface. We also keep giving regular updates so stay tuned! ',
+            image:'',
+            link:'https://www.cynuxsecurity.org/'
             
         },
 
        {
             title:'Megham Cloud',
-            description:'We are working on building a super computer that will be able to host developers, apps, CTF platforms, game engines and virtual machines within NITW',
+            message:'We are working on building a super computer that will be able to host developers, apps, CTF platforms, game engines and virtual machines within NITW',
             image:
             
         },
 
        {
             title:'LAN Applications',
-            description:'SSH messenger (for secure and encrypted texting) and LAN radio were developed for use within the LAN network',
+            message:'SSH messenger (for secure and encrypted texting) and LAN radio were developed for use within the LAN network',
             image:
             
         },
 
        {
             title:'Hardware Projects',
-            description:'Several hardware projects were developed using BadUSB and NodeMCU',
+            message:'Several hardware projects were developed using BadUSB and NodeMCU',
             image:
             
         },
 
        {
             title:'Honeypot Server',
-            description:'Honeypot Server is a cyberattack protection system developed specially for NIT, Warangal.',
+            message:'Honeypot Server is a cyberattack protection system developed specially for NIT, Warangal.',
             image:
             
         },
 
        {
             title:'BugClaim',
-            description:'BugClaim is a startup under the parent company Protonull Corporation Pvt Ltd. It is a bugbounty platform where security researchers identify and submit vulnerability reports regarding websites, products, services etc. ',
-            image:
+            message:'BugClaim is a startup under the parent company Protonull Corporation Pvt Ltd. It is a bugbounty platform where security researchers identify and submit vulnerability reports regarding websites, products, services etc. ',
+            image:'',
+            link:'https://www.bugclaim.com/'
             
         }
   ];
