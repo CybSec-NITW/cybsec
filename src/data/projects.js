d8997b52547e36f2bfeb4d552a06b7fa940362f6
Ruchira-111 <53949768+Ruchira-111@users.noreply.github.com>
2021-03-14 22:10:21 +0530

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+module.exports = [
+  
+         {
+            title:'Cynux Security OS',
+            description:'Cynux is a Linux based distribution curated for pentesters with more than 200 tools and a great interface. We also keep giving regular updates so stay tuned! ',
+            image:
+            
+        },
+
+       {
+            title:'Megham Cloud',
+            description:'We are working on building a super computer that will be able to host developers, apps, CTF platforms, game engines and virtual machines within NITW',
+            image:
+            
+        },
+
+       {
+            title:'LAN Applications',
+            description:'SSH messenger (for secure and encrypted texting) and LAN radio were developed for use within the LAN network',
+            image:
+            
+        },
+
+       {
+            title:'Hardware Projects',
+            description:'Several hardware projects were developed using BadUSB and NodeMCU',
+            image:
+            
+        },
+
+       {
+            title:'Honeypot Server',
+            description:'Honeypot Server is a cyberattack protection system developed specially for NIT, Warangal.',
+            image:
+            
+        },
+
+       {
+            title:'BugClaim',
+            description:'BugClaim is a startup under the parent company Protonull Corporation Pvt Ltd. It is a bugbounty platform where security researchers identify and submit vulnerability reports regarding websites, products, services etc. ',
+            image:
+            
+        }
+  ];
