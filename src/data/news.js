ab0a6c6ab0585d7162f874656c760f3cce8e305f
Aditya Srivastava <iamaditya2009@gmail.com>
2021-10-13 22:27:32 +0530

    site updated

--- src/data/news.js
+++ src/data/news.js
@@ -2,174 +2,398 @@
 // First items are the latest.
 
 module.exports = [
-        {
-            title: '$root',
-            message:'I am Groot!!!🙃🙂\nHacker | Coder | Web Developer | Programmer | Memer | Foodie | Omniscient HackTheBox | Iron Man Fan | Night-Owl | TIFLS | Interested in Astrophysics | Jack of All Trades | South Delhi | O -ve\nWe break security not hearts',
-            image:'/images/groot.jpg',
-            link:'https://aakash.cybsec.in'
-        },
-        {
-            title:'Sachiko',
-            message:'Hii this is Sachiko\nDont be fooled by that face, she will hack you',
-            image:'/images/Sachiko.jpg',
-            link:'https://argenestel.github.io'
-        },
-        {
-            title:'Sp1ynt3r',
-            message:'Mind Your own HACKING',
-            image:'/images/Sp1ynt3r.jpeg',
-            link:'#'
-        },
-        {
-            title:'Phantom King',
-            message:'できないことはできる！バグを食べるのが大好きです。あなたは私が何を計画しているのか分かりません！',
-            image:'/images/PhantomKing.jpg',
-            link:'#'
-        },
-        {
-            title:'MY$TIQU3',
-            message:'Hey its mystique... \nWhen solving problems, dig at the roots instead of just hacking at the leaves.',
-            image:'/images/MY$TIQU3.jpeg',
-            link:'#'
-        },
-        {
-            title:'Argenestel',
-            message:'Otaku in hackers realm',
-            image:'/images/Argenestel.jpg',
-            link:'#'
-        },
-        {
-            title:'Acid',
-            message:'Not basic...\nPlaying for keeps ',
-            image:'/images/acid.jpg',
-            link:'https://yash.cybsec.in'
-        },
-        {
-            title:'a.chick',
-            message:"I don't chicken out",
-            image:'/images/ashiqa.jpeg',
-            link:'http://www.ashiqarahman.me'
-        },
-        {
-            title:'Dr. Sheldon',
-            message:"Ohh you all must be knowing me. If not, then go away you inferior brains.\nBTW, I'm batman!!!! Bazinga!!!",
-            image:'/images/DrSheldon.jpg',
-            link:'#'
-        },
-        {
-            title:'B.O.B.',
-            message:' Bellow... This is BoB.. Buster of Bugs..\nMy job is thinking out of the bob.',
-            image:'/images/bob.jpeg',
-            link:'#'
-        },
-        {
-           title:'D1SQU1TA',
-           message:'I will find you, and I will OSINT you',
-           image:'/images/F1R3H3ART.jpg',
-           link:'#'
-        },
-        {
-            title:'Kakrotgnan',
-            message:'Lemme touch U...\nYour weakness is my strength 💪',
-            image:'/images/Kakrotgnan.jpeg',
-            link:'#'
-        },
-        {
-            title:'Pika',
-            message:'Hello this is detective Pikachu\nBEWARE! Ur poke balls could be hacked.',
-            image:'/images/pica_pica.jpeg',
-            link:'https://harika140901.github.io/'
-        },
-        {
-            title:'Dคяк☬Kห¡gнт',
-            message:"It's Not Who i am underneath ,\nBut What I Hack that defines me",
-            image:'/images/Dคяк☬Kห¡gнт.png',
-            link:'#'
-        },
-        {
-            title:'m3g4n',
-            message:"I'm Megan.\nHacking to learn",
-            image:'/images/megan.jpeg',
-            link:'#'
-        },
-        {
-            title:'Tommy shelby',
-            message:'Intelligence is a very valuable thing, innit, my friend? And usually it comes far too   late.',
-            image:'/images/TommyShelby.jpg',
-            link:'https://www.linkedin.com/in/nagendra-vinay-bab6221a6'
-        },
-        {
-            title:'DARK DRAGON',
-            message:'INFOrmation is Wealth',
-            image:'/images/DARKDRAGON.jpeg',
-            link:'#'
-        },
-        {
-            title:'Purplert',
-            message:'Please stand by...\nOr get ready to become victim of social engineering',
-            image:'/images/scarlett.jpeg',
-            link:'https://www.rashmika.me'
-        },
-        {
-            title:'Lucy',
-            message:'Your Ignorance is our power',
-            image:'/images/lucy.jpeg',
-            link:'#'
-        },
-        {
-            title:'LakuMigAn0',
-            message:'Hey there! 👋\nAspiring Hacker & Coder | Music Love\nNever tell anyone everything you know... ;)',
-            image:'/images/LakuMigAn0.jpeg',
-            link:'https://github.com/Lakshaya18'
-        },
-        {
-            title:'TrivalenT',
-            message:'Wubba Lubba Dub Dub 🤤\nMixing potions to hack u wid my chemistry 🧙‍♂‍',
-            image:'/images/TrivalenT.jfif',
-            link:'https://shubhank.netlify.app'
-        },
-        {
-            title:'Neo',
-            message:'Hey! I am Neo. Nice to m33t u.',
-            image:'/images/Neo.jpg',
-            link:'#'
-        },
-        {
-            title:'Harlequin',
-            message:"Hi, I'm harlequin.\nDon't hate the hacker, hate the code.",
-            image:'/images/Harlequin.jpg',
-            link:'#'
-        },
-        {
-            title:'Entwicklerish',
-            message:'Sugar rush proximity detected, how would you like to proceed?',
-            image:'/images/Entwicklerish.png',
-            link:'#'
-        },
-        {
-            title:'Anonymous',
-            message:"Hello, I'm Anonymous\nI like to lay low\nPrivacy is a myth",
-            image:'/images/Anonymous.jpg',
-            link:'#'
-        },
-        {
-            title: '5and0r D3zt3z',
-            message: `This is Sandor
+  {
+    title: "$root",
+    message:
+      "I am Groot!!!🙃🙂\nHacker | Coder | Web Developer | Programmer | Memer | Foodie | Omniscient HackTheBox | Iron Man Fan | Night-Owl | TIFLS | Interested in Astrophysics | Jack of All Trades | South Delhi | O -ve\nWe break security not hearts",
+    image: "/images/groot.jpg",
+    link: "https://aakash.cybsec.in",
+  },
+  {
+    title: "Sachiko",
+    message:
+      "Hii this is Sachiko\nDont be fooled by that face, she will hack you",
+    image: "/images/Sachiko.jpg",
+    link: "https://argenestel.github.io",
+  },
+  {
+    title: "Sp1ynt3r",
+    message: "Mind Your own HACKING",
+    image: "/images/Sp1ynt3r.jpeg",
+    link: "#",
+  },
+  {
+    title: "Phantom King",
+    message:
+      "できないことはできる！バグを食べるのが大好きです。あなたは私が何を計画しているのか分かりません！",
+    image: "/images/PhantomKing.jpg",
+    link: "#",
+  },
+  {
+    title: "MY$TIQU3",
+    message:
+      "Hey its mystique... \nWhen solving problems, dig at the roots instead of just hacking at the leaves.",
+    image: "/images/MY$TIQU3.jpeg",
+    link: "#",
+  },
+  {
+    title: "Argenestel",
+    message: "Otaku in hackers realm",
+    image: "/images/Argenestel.jpg",
+    link: "#",
+  },
+  {
+    title: "Acid",
+    message: "Not basic...\nPlaying for keeps ",
+    image: "/images/acid.jpg",
+    link: "https://yash.cybsec.in",
+  },
+  {
+    title: "a.chick",
+    message: "I don't chicken out",
+    image: "/images/ashiqa.jpeg",
+    link: "http://www.ashiqarahman.me",
+  },
+  {
+    title: "Dr. Sheldon",
+    message:
+      "Ohh you all must be knowing me. If not, then go away you inferior brains.\nBTW, I'm batman!!!! Bazinga!!!",
+    image: "/images/DrSheldon.jpg",
+    link: "#",
+  },
+  {
+    title: "B.O.B.",
+    message:
+      " Bellow... This is BoB.. Buster of Bugs..\nMy job is thinking out of the bob.",
+    image: "/images/bob.jpeg",
+    link: "#",
+  },
+  {
+    title: "D1SQU1TA",
+    message: "I will find you, and I will OSINT you",
+    image: "/images/F1R3H3ART.jpg",
+    link: "#",
+  },
+  {
+    title: "Kakrotgnan",
+    message: "Lemme touch U...\nYour weakness is my strength 💪",
+    image: "/images/Kakrotgnan.jpeg",
+    link: "#",
+  },
+  {
+    title: "Pika",
+    message:
+      "Hello this is detective Pikachu\nBEWARE! Ur poke balls could be hacked.",
+    image: "/images/pica_pica.jpeg",
+    link: "https://harika140901.github.io/",
+  },
+  {
+    title: "Dคяк☬Kห¡gнт",
+    message: "It's Not Who i am underneath ,\nBut What I Hack that defines me",
+    image: "/images/Dคяк☬Kห¡gнт.png",
+    link: "#",
+  },
+  {
+    title: "m3g4n",
+    message: "I'm Megan.\nHacking to learn",
+    image: "/images/megan.jpeg",
+    link: "#",
+  },
+  {
+    title: "Tommy shelby",
+    message:
+      "Intelligence is a very valuable thing, innit, my friend? And usually it comes far too   late.",
+    image: "/images/TommyShelby.jpg",
+    link: "https://www.linkedin.com/in/nagendra-vinay-bab6221a6",
+  },
+  {
+    title: "DARK DRAGON",
+    message: "INFOrmation is Wealth",
+    image: "/images/DARKDRAGON.jpeg",
+    link: "#",
+  },
+  {
+    title: "Purplert",
+    message:
+      "Please stand by...\nOr get ready to become victim of social engineering",
+    image: "/images/scarlett.jpeg",
+    link: "https://www.rashmika.me",
+  },
+  {
+    title: "Lucy",
+    message: "Your Ignorance is our power",
+    image: "/images/lucy.jpeg",
+    link: "#",
+  },
+  {
+    title: "LakuMigAn0",
+    message:
+      "Hey there! 👋\nAspiring Hacker & Coder | Music Love\nNever tell anyone everything you know... ;)",
+    image: "/images/LakuMigAn0.jpeg",
+    link: "https://github.com/Lakshaya18",
+  },
+  {
+    title: "TrivalenT",
+    message:
+      "Wubba Lubba Dub Dub 🤤\nMixing potions to hack u wid my chemistry 🧙‍♂‍",
+    image: "/images/TrivalenT.jfif",
+    link: "https://shubhank.netlify.app",
+  },
+  {
+    title: "Neo",
+    message: "Hey! I am Neo. Nice to m33t u.",
+    image: "/images/Neo.jpg",
+    link: "#",
+  },
+  {
+    title: "Harlequin",
+    message: "Hi, I'm harlequin.\nDon't hate the hacker, hate the code.",
+    image: "/images/Harlequin.jpg",
+    link: "#",
+  },
+  {
+    title: "Entwicklerish",
+    message: "Sugar rush proximity detected, how would you like to proceed?",
+    image: "/images/Entwicklerish.png",
+    link: "#",
+  },
+  {
+    title: "Anonymous",
+    message: "Hello, I'm Anonymous\nI like to lay low\nPrivacy is a myth",
+    image: "/images/Anonymous.jpg",
+    link: "#",
+  },
+  {
+    title: "5and0r D3zt3z",
+    message: `This is Sandor
             I hack for money ...Bug Bounty...`,
-            image: '/images/sandor.png',
-            link: '#'
-        },
-        {
-            title: 'tonyq16',
-            message: `You know who I am.
+    image: "/images/sandor.png",
+    link: "#",
+  },
+  {
+    title: "tonyq16",
+    message: `You know who I am.
             Currently making scripts to hack you, be prepared`,
-            image: '/images/tony.jpeg',
-            link: '#'
-        },
-        {
-                title: 'Omen',
-                message: 'I am the beginning. I am the end.',
-                image: '/images/omen.png',
-                link: '#'
-        }
+    image: "/images/tony.jpeg",
+    link: "#",
+  },
+  {
+    title: "Omen",
+    message: "I am the beginning. I am the end.",
+    image: "/images/omen.png",
+    link: "#",
+  },
+  {
+    title: "Hanipasha",
+    message: "Bug is cool until it's in code",
+    image: "/images/Hanipasha.jpg",
+    link: "#",
+  },
+  { title: "Falcon", message: ".", image: "/images/Falcon.jpg", link: "#" },
+  {
+    title: "No one",
+    message: "Powerhouse @mitochondria",
+    image: "/images/No_one.jpeg",
+    link: "#",
+  },
+  {
+    title: "Phenix",
+    message: "PeaceÔ£î",
+    image: "/images/Phenix.jpeg",
+    link: "#",
+  },
+  {
+    title: "Cipher",
+    message: "Amateurs hack systems,\nProfessionals hack people.",
+    image: "/images/Cipher.png",
+    link: "#",
+  },
+  {
+    title: "Oboy",
+    message: "Oh boy!! We got a shell.\n...\n...\nA rabbit hole, oh boy'.",
+    image: "/images/Oboy.jpg",
+    link: "#",
+  },
+  {
+    title: "saitama s├¿├▒s├®├¡",
+    message: "Hello I am saitama!!!\nRank 39 Class A",
+    image: "/images/saitama_s├¿├▒s├®├¡.jpeg",
+    link: "#",
+  },
+  {
+    title: "Mr.V",
+    message: "Humans stupidity, That's why hackers always win.",
+    image: "/images/Mr.V.jpeg",
+    link: "#",
+  },
+  {
+    title: "k1Ts8n",
+    message:
+      "Early bird gets the worm,\nEarly fox gets the bird,\nThen comes the . . . . \nDecode my name please\xadƒÿë",
+    image: "/images/k1Ts8n.jpg",
+    link: "#",
+  },
+  {
+    title: "Cyber Penguin",
+    message: "I am a learner",
+    image: "/images/Cyber_Penguin.jpeg",
+    link: "#",
+  },
+  { title: "LSD", message: "PeaceÔ£î", image: "/images/LSD.jpeg", link: "#" },
+  {
+    title: "neohat or neo",
+    message: "l0v3 to h4ck!",
+    image: "/images/neohat_or_neo.jpeg",
+    link: "#",
+  },
+  {
+    title: "banksy",
+    message: "Hacking comforts the disturbed and disturbs the comfortable",
+    image: "/images/banksy.jpg",
+    link: "#",
+  },
+  {
+    title: "Yeah",
+    message: "Hack the hack",
+    image: "/images/Yeah.jpeg",
+    link: "#",
+  },
+  {
+    title: "Sphinx_x",
+    message: "Tech-savvytastic!",
+    image: "/images/Sphinx_x.jpg",
+    link: "#",
+  },
+  {
+    title: "godofmischief",
+    message: "Your saviour Is here!",
+    image: "/images/godofmischief.jpg",
+    link: "#",
+  },
+  {
+    title: "anstronium",
+    message: "Data is the the new oil",
+    image: "/images/anstronium.jpeg",
+    link: "#",
+  },
+  {
+    title: "Slick",
+    message:
+      "Soy Slick. Yo escribo para el equipo. Yo escribo para tu seguridad.",
+    image: "/images/Slick.jpg",
+    link: "#",
+  },
+  {
+    title: "av3ng3r",
+    message: "You thought your secrets were safe. You were wrong",
+    image: "/images/av3ng3r.jpg",
+    link: "#",
+  },
+  {
+    title: "no_name",
+    message: "fastest hand in the west; coming to score that big bounty",
+    image: "/images/no_name.jpg",
+    link: "#",
+  },
+  {
+    title: "Lucy",
+    message: "There's no right or wrong. There's only fun and boring",
+    image: "/images/Lucy.jpg",
+    link: "#",
+  },
+  {
+    title: "n00Bm4n",
+    message:
+      "Hacking is not a crime, it's a profession till the time you play with it safely. So, stay away from me. \xadƒÿê\xadƒÿê",
+    image: "/images/n00Bm4n.jpg",
+    link: "#",
+  },
+  {
+    title: "Disquita",
+    message:
+      "Hacker \\| Coder \\| Web Developer \\| CyberChef\nI will find you, and I will OSINT you...",
+    image: "/images/Disquita.jpg",
+    link: "#",
+  },
+  {
+    title: "0R4CL3_H3R3",
+    message: "'@[add_text_here]",
+    image: "/images/0R4CL3_H3R3.jpeg",
+    link: "#",
+  },
+  {
+    title: "S3raphinA",
+    message:
+      "I'm good at reading people\nMy secret, I look for the worst in them!!",
+    image: "/images/S3raphinA.jpg",
+    link: "#",
+  },
+  {
+    title: "Disquita",
+    message:
+      "Hacker \\| Coder \\| Web Developer \nI will find you, and I will OSINT you...",
+    image: "/images/Disquita.png",
+    link: "#",
+  },
+  {
+    title: "Keerthi",
+    message: "Think you know me ? Well...Think again",
+    image: "/images/Keerthi.jpg",
+    link: "#",
+  },
+  {
+    title: "Bruce Lee",
+    message: "Using no way as a way, having no limitation as limitation",
+    image: "/images/Bruce_Lee.jpg",
+    link: "#",
+  },
+  {
+    title: "Cyborg0403",
+    message: "No computer is secure",
+    image: "/images/Cyborg0403.jpg",
+    link: "#",
+  },
+  {
+    title: "Mr.V",
+    message: "Human Stupidity, that's why Hackers always win.",
+    image: "/images/Mr.V.jpeg",
+    link: "#",
+  },
+  {
+    title: "Cybsquader",
+    message: "If you want to change your life change your mind",
+    image: "/images/Cybsquader.jpeg",
+    link: "#",
+  },
+  {
+    title: "m3g4n",
+    message: "Hacking to learn.",
+    image: "/images/m3g4n.jpg",
+    link: "#",
+  },
+  {
+    title: "Sudonymous",
+    message:
+      "I'm good at reading people. My secret? I look for the worst in them",
+    image: "/images/Sudonymous.jpg",
+    link: "#",
+  },
+  {
+    title: "Sp1ynt3r",
+    message: "Mind your own Hacking",
+    image: "/images/Sp1ynt3r.jpeg",
+    link: "#",
+  },
+  {
+    title: "L3x 1uth0r",
+    message: "Hacker \\| Developer \\| Researcher",
+    image: "/images/L3x_1uth0r.jpg",
+    link: "#",
+  },
+  {
+    title: "Acid",
+    message: "Not the basic guy",
+    image: "/images/Acid.jpg",
+    link: "#",
+  },
 ];
